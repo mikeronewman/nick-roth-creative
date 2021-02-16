6117c6690ad539cad59df07f15a68ac7082874e4
Michael Newman <mikeronewman@gmail.com>
2021-02-15 20:56:03 -0500

    :sparkles: Add netlify fields for Photos collection

--- js/main.js
+++ js/main.js
@@ -2,8 +2,14 @@
 import CMS from 'netlify-cms-app'
 // Initialize the CMS object
 CMS.init();
+
+//---- Import cloudinary ----//
+import cloudinary from 'netlify-cms-media-library-uploadcare';
+
 // Now the registry is available via the CMS object.
-// CMS.registerPreviewTemplate('my-template', MyTemplate);
+CMS.registerMediaLibrary(cloudinary);
+
+
 
 // Hamburger and mobile menu animations
 const hamburger = document.getElementById('hamburger');
