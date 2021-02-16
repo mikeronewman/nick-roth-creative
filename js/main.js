466abd142f72ef95e493b7948a2acf43aee6d9b5
Michael Newman <mikeronewman@gmail.com>
2021-02-15 21:57:46 -0500

    trying to get netlifycms configured correctly

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 //---- Import netlify-cms ----//
 import CMS from 'netlify-cms-app'
-// Initialize the CMS object
-CMS.init();
 
 //---- Import cloudinary ----//
-import cloudinary from 'netlify-cms-media-library-uploadcare';
+import cloudinary from 'netlify-cms-media-library-cloudinary';
 
 // Now the registry is available via the CMS object.
 CMS.registerMediaLibrary(cloudinary);
 
+// Initialize the CMS object
+CMS.init();
 
 
 // Hamburger and mobile menu animations
