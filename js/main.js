6c51e5fa3d571a333fc27c5e034562ab491d4141
Michael Newman <mikeronewman@gmail.com>
2021-02-15 20:34:21 -0500

    Begin configuring netlify, and add build folder to .gitignore to build at deploy

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-// //---- Import netlify-cms ----//
-// import CMS from 'netlify-cms-app'
-// // Initialize the CMS object
-// CMS.init();
-// // Now the registry is available via the CMS object.
+//---- Import netlify-cms ----//
+import CMS from 'netlify-cms-app'
+// Initialize the CMS object
+CMS.init();
+// Now the registry is available via the CMS object.
 // CMS.registerPreviewTemplate('my-template', MyTemplate);
 
 // Hamburger and mobile menu animations
