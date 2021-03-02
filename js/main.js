03dd663170d82bc6ec5e4ff9d0b83c2d467e79d4
Michael Newman <mikeronewman@gmail.com>
2021-03-02 13:56:50 -0500

    Add very very rough carousel implementation

--- js/main.js
+++ js/main.js
@@ -28,26 +28,61 @@ hamburger.onclick = function() {
 // CAROUSEL STUFF
 const prevBtn = document.getElementById('carouselPrev');
 const nextBtn = document.getElementById('carouselNext');
+const slides = document.getElementsByClassName('carousel__image');
 let currentSlide = document.getElementById('currentSlide');
 let nextSlide = document.getElementById('currentSlide').nextElementSibling;
 
 const showPreviousSlide = (e) => {
-    console.log(e);
+    if(currentSlide == slides[0]) {
+        currentSlide.classList.toggle('carousel__image--active');
+        currentSlide.id = "";
+        console.log(slides);
+        currentSlide = slides[slides.length - 1];
+        console.log(currentSlide);
+        currentSlide.id = "currentSlide";
+        currentSlide.classList.toggle('carousel__image--active');
+    } else if (currentSlide == slides[1]) {
+        currentSlide.classList.toggle('carousel__image--active');
+        currentSlide.id = "";
+        currentSlide = slides[0];
+        currentSlide.id = "currentSlide";
+        currentSlide.classList.toggle('carousel__image--active');
+    } else if (currentSlide == slides[2]) {
+        currentSlide.classList.toggle('carousel__image--active');
+        currentSlide.id = "";
+        currentSlide = slides[1];
+        currentSlide.id = "currentSlide";
+        currentSlide.classList.toggle('carousel__image--active');
+
+    }
 }
 
 const showNextSlide = (e) => {
-    console.log(e);
-    console.log(currentSlide);
-    console.log(nextSlide);
+    if(nextSlide.classList.contains('carousel__image')) {
+        currentSlide.classList.toggle('carousel__image--active');
+        nextSlide.classList.toggle('carousel__image--active');
+        currentSlide.id = "";
+        nextSlide.id = "currentSlide";
+        currentSlide = document.getElementById('currentSlide');
+        nextSlide = document.getElementById('currentSlide').nextElementSibling;
+    } else {
+        currentSlide.classList.toggle('carousel__image--active');
+        currentSlide.id = "";
+        currentSlide = slides[0];
+        currentSlide.id = "currentSlide";
+        currentSlide.classList.toggle('carousel__image--active');
+        nextSlide = slides[1];
+    }
 
-    currentSlide.classList.toggle('carousel__image--active');
-    nextSlide.classList.toggle('carousel__image--active');
-    currentSlide.id = "";
-    nextSlide.id = "currentSlide";
-    currentSlide = document.getElementById('currentSlide');
-    nextSlide = document.getElementById('currentSlide').nextElementSibling;
+    automateCarousel();
+}
+
+const automateCarousel = () => {
+    setInterval(showNextSlide, 5000);
 }
 
 prevBtn.addEventListener('click', showPreviousSlide);
 nextBtn.addEventListener('click', showNextSlide);
+
+automateCarousel();
 
